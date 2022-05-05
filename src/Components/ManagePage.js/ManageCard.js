484b37e48333034d6cfa6a92be3b03baf1c79218
md-moniruzzaman01 <md.moniruzzaman1050@gmail.com>
2022-05-05 19:19:20 +0600

    manage page design & delete api added

--- src/Components/ManagePage.js/ManageCard.js
+++ src/Components/ManagePage.js/ManageCard.js
@@ -1,25 +1,23 @@
-import axios from 'axios';
+
 import React, { useEffect } from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
-const ManageCard = () => {
-    useEffect(()=>{
-        axios.get('http://localhost:5000/all')
-    },[])
+const ManageCard = ({item,deleteHandle}) => {
+    const {_id,title,email,image,price,supplierName}=item;
     return (
         <div>
-        <div class="flex relative my-5   mx-auto  items-center  rounded-lg  shadow-md md:flex-row md:max-w-xl  border-gray-700 bg-gray-200 ">
-<img class=" h-20 m-2 w-auto rounded-r-lg" src='https://i.ibb.co/T2TS9hG/mastering-the-kitchen-572x764-1-550x680.jpg' alt=""/>
-<div class="flex  justify-between leading-normal w-6/12">
+        <div class="flex relative my-5   mx-auto  items-center  rounded-lg  shadow-md md:flex-row md:max-w-2xl  border-gray-700 bg-gray-200 ">
+<img class=" h-28 m-2 w-auto rounded-r-lg md:h-20" src={image} alt=""/>
+<div class=" block justify-between leading-normal w-8/12 md:flex">
    <div>
-   <h5 class=" text-sm font-bold tracking-tight  text-gray-700 md:text-base">servieName</h5>
-    <p class=" text-sm  text-gray-400">By <span>admin@gmail.com</span></p>
+   <h5 class="  font-bold tracking-tight  text-gray-700 ">{title.slice(0,29)}..</h5>
+    <p class=" text-sm  text-gray-400">By <span>{email.slice(0,20)}...</span></p>
     
    </div>
    <div>
-       <p className='text-gray-500 text-sm md:text-base'>price:</p>
-       <p className='text-gray-500 text-sm md:text-base'>Supplier:</p>
+       <p className='text-gray-500 text-sm md:text-base'>price: ${price} </p>
+       <p className='text-gray-500 text-sm md:text-base'>Supplier: {supplierName.slice(0,10)}..</p>
    </div>
-    <div  className='text-white flex items-center justify-center absolute inset-y-1/3 right-1 px-2 py-2.5 bg-gray-400 rounded-full text-xl'>
+    <div onClick={()=>deleteHandle(_id)} className='text-white flex items-center justify-center absolute inset-y-1/3 right-1 px-1.5 py-2 bg-gray-400 rounded-full text-xl'>
     
     <FaTrashAlt />
 </div>
--- src/Components/ManagePage.js/ManagePage.js
+++ src/Components/ManagePage.js/ManagePage.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ManageCard from './ManageCard';
-
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 const ManagePage = () => {
+    const navigate = useNavigate()
+    const [allitems ,setallitems]=useState([])
+    useEffect(()=>{
+        axios.get('http://localhost:5000/all')
+        .then(Response=>{
+            const items =(Response.data);
+            setallitems(items)
+        })
+    },[])
+    const additembtnHandle = ()=>{
+        navigate('/add')
+    }
+    const deleteHandle =(id)=>{
+            console.log(id);
+            fetch(`http://localhost:5000/remove/${id}`,{
+                method: 'DELETE'
+            })
+            .then(res=> res.json())
+            .then(data=>{
+                if(data.deletedCount>0){
+                    const rest = allitems.filter(item => item._id !==id)
+                    setallitems(rest)
+                }
+            })
+            
+    }
     return (
         <div>
-        <ManageCard></ManageCard>
+           <div className='flex justify-center mt-5'> <button onClick={additembtnHandle} className='rounded w-6/12 max-w-[500px] min-w-[100px]  py-2 font-semibold text-gray-100 bg-green-400'>Add item</button></div>
+            {
+                allitems.map(item => <ManageCard item={item} deleteHandle={deleteHandle}></ManageCard>)
+            }
+        
         </div>
     );
 };
