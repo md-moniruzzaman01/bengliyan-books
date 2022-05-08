e4effbb9945b4f5614e6273d3981a1cd0fc10241
md-moniruzzaman01 <md.moniruzzaman1050@gmail.com>
2022-05-08 13:47:20 +0600

    set blogs page with mongo database

--- src/Components/BlogsPage/Blog.js
+++ src/Components/BlogsPage/Blog.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Blog = ({blg,Detailsbtn}) => {
-    const {id,img,name,body,comments,tags} =blg;
+    const [read,setRead]= useState(false)
+    const {_id,img,name,body,comments,tags} =blg;
+    const shortdata = body.slice(0,100)
+    const [data,setData]= useState(shortdata)
+ 
+  
     return (
    
-            <div id={id}  className='   shadow-md  mb-11 mt-7 py-5 p-3 max-w-3xl mx-auto rounded-md'>
+            <div id={_id}  className='   shadow-md  mb-11 mt-7 py-5 p-3 max-w-3xl mx-auto rounded-md'>
         <div className='w-full relative'>
             <img className='w-full h-full rounded-sm' src={img} alt="" />
             {/* <p className='px-5 rounded-md text-lg text-white   py-5 left-4 absolute bottom-[-20px] bg-red-600'>jan 15</p> */}
         </div>
         <div className='relative'>
             <h1 className='text-gray-800 mt-7 text-2xl'>{name}</h1>
-            <p className='text-gray-700 mt-5 pb-16'>{body.slice(0,100)}... <span className='text-sky-700 underline'>read more</span></p>
+            <p className='text-gray-700 mt-5 pb-16'>{read? body : shortdata}... <span onClick={()=>setRead(!read)} className='text-sky-700 underline cursor-pointer'>{read? 'real less' : "read more"}</span></p>
             <div className='flex text-gray-500 absolute bottom-1'>
             <p className=' border-r-2 border-gray-300  '>  <span className='px-2'>{tags}</span></p>
             <p className='ml-2'> <span className='ml-2'>{comments} Comments</span></p>
--- src/Components/BlogsPage/Blogs.js
+++ src/Components/BlogsPage/Blogs.js
@@ -1,33 +1,36 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Blog from './Blog';
 import '../Style/blogStyle.css'
 
-// import firstImg from '../../image/authentication-vs-authorization.png'
-// import secImg from '../../image/firebase1200.jpg'
-// import thrdImg from '../../image/images.png'
+
 
 
 const Blogs = () => {
+    const [blogs ,setBlogs]=useState([])
     const navigate = useNavigate()
-    const blogs =[
-        {name:'What is difference between authorization and authentication?', body:'Authorization in system security is the process of giving the user permission to access a specific resource.In simple terms, authentication is the process of verifying who a user is.Both Authentication and Authorization area unit utilized in respect of knowledge security that permits the safety on an automatic data system.In authentication process, the identity of users are checked for providing the access to the system.Simply put, authentication is the process of verifying who someone is, whereas authorization is the process of verifying what specific applications, files, and data a user has access to.',tags:'Travel, lifestyle', id:'1',comments:'3'},
-        {name:' Why are you using firebase? What other options do you have to implement authentication?', body:"Firebase is a web app  development platform own by Google.Google Firebase offers many features that pitch it as the go-to backend development tool for web and mobile apps. It reduces development workload and time. And it's a perfect prototyping tool. Firebase is simple, lightweight, friendly, and industrially recognized.It allows built in email/password authentication system. It also supports OAuth2 for Google,Facebook, Twitter and GitHub. In new days we have some alternative of firebase authentication like Okta,0auth etc.but for beganing firebase is best option to start . ",tags:'Travel, lifestyle', id:'2',comments:'3'},
-        {name:'What other services does firebase provide other than authentication', body:"Firebase is a awesome tool to developer's. it provied some services like Realtime Database, File Storage , Authentication and Hosting . firebase file storage  provides a simple way to save binary files — most often images, but it could be anything — to Google Cloud Storage directly from the client!!! .Firebase includes an easy-to-use hosting service for all of your static files. It serves them from a global CDN with HTTP/2.",tags:'Travel, lifestyle', id:'3',comments:'3'},
+    useEffect(()=>{
         
-    ]
+      fetch('https://guarded-dusk-72997.herokuapp.com/blogs')
+      .then(res=> res.json())
+      .then(data=>{
+        setBlogs(data)
+          
+      })
+     
+    },[])
 const Detailsbtn =(id)=>{
         navigate(`/blogs/${id}`)
 }
-
+console.log(blogs);
 
     return (
       <div className=''>
             <div className='blog-section container mx-auto'>
          <div>
          {
-             blogs.map(blg=> <Blog Detailsbtn={Detailsbtn} key={blg.id} blg={blg}></Blog>)
+             blogs.map(blg=> <Blog Detailsbtn={Detailsbtn} key={blg._id} blg={blg}></Blog>)
          }
          </div>
         
@@ -35,7 +38,7 @@ const Detailsbtn =(id)=>{
        <div className='category w-full  md:w-10/12  bg-gray-100 min-h-[90vh]  text-gray-800 mx-auto rounded   '>
             <h1 className='text-gray-800 text-2xl border-b-2 border-gray-400'>Category</h1>
            {
-               blogs.map(bgheader =>  <p className='mt-5 border-b-small py-2'> <a href={`#${bgheader.id}`}>{bgheader.name}</a>  </p>
+               blogs.map(bgheader =>  <p className='mt-5 border-b-small py-2'> <a href={`#${bgheader._id}`}>{bgheader.name}</a>  </p>
                )
            }
         </div>
